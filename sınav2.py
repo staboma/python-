721502e98ea40d3e51993699d7a6cd8be023b32f
staboma <163428917+staboma@users.noreply.github.com>
2024-05-23 15:23:23 +0300

    Add files via upload

--- sınav2.py
+++ sınav2.py
@@ -1,23 +1,18 @@
-sorular = {
-    "1. Soru": "Türkiye'nin başkenti?",
-    "2. Soru": "En yüksek dağ?",
-    "3. Soru": "Kim bilir?",
-}
-
-cevaplar = {
-    "1. Soru": "Ankara",
-    "2. Soru": "Everest",
-    "3. Soru": "Yaradan bilir.",
+sorularVeDogruCevaplari = {
+    "1. Soru: Türkiye'nin başkenti hangi ildir?": "Ankara",
+    "2. Soru: En yüksek dağın adı nedir?": "Everest",
+    "3. Soru: Kim bilir": "Yaradan bilir"
 }
 
 puan = 0
-for soru, dogru_cevap in sorular.items():
+
+for soru, dogru_cevap in sorularVeDogruCevaplari.items():
     cevap = input(soru + "\nCevabınız: ")
-    if cevap.lower() == dogru_cevap.lower():
+    if dogru_cevap.lower() in cevap.lower():
         print("Doğru!")
         puan += 10
     else:
         print("Yanlış!")
 
-print("Toplam Puanınız:", puan)
+print("Toplam Puanınız: ", puan)
 
