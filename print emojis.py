1c2ba6c96e87674b475150754c766b8efe34b585
staboma <163428917+staboma@users.noreply.github.com>
2024-06-02 19:24:07 +0300

    Add files via upload

---
+++ print emojis.py
@@ -0,0 +1,39 @@
+import emoji
+
+def print_unicode_emojis():
+    print("\U0001F600")  # 😀 Gülümseyen yüz
+    print("\U0001F603")  # 😃 Gülen yüz
+    print("\U0001F604")  # 😄 Gözleri kapalı gülen yüz
+    print("\U0001F601")  # 😁 Gülümseyen gözler
+    print("\U0001F606")  # 😆 Gözleri kapalı gülen
+    print("\U0001F605")  # 😅 Soğuk ter
+    print("\U0001F602")  # 😂 Gözyaşlarıyla gülmek
+    print("\U0001F923")  # 🤣 Yere yatıp gülmek
+    print("\U0001F60A")  # 😊 Mutlu
+    print("\U0001F607")  # 😇 Melek
+    print("\U0001F970")  # 🥰 Kalpli gözler
+    print("\U0001F60D")  # 😍 Kalpli gözler
+    print("\U0001F618")  # 😘 Öpücük atmak
+    print("\U0001F617")  # 😗 Öpücük
+    print("\U0001F619")  # 😙 Gözleri kapalı öpücük
+    print("\U0001F61A")  # 😚 Gözleri kapalı öpücük
+    print("\U0001F917")  # 🤗 Sarılmak
+    print("\U0001F929")  # 🤩 Yıldızlı gözler
+    print("\U0001F60B")  # 😋 Leziz
+    print("\U0001F61B")  # 😛 Dil çıkarmak
+    print("\U0001F61C")  # 😜 Göz kırparak dil çıkarmak
+    print("\U0001F61D")  # 😝 Gözleri kapalı dil çıkarmak
+    print("\U0001F911")  # 🤑 Para gözler
+    print("\U0001F910")  # 🤐 Ağzı fermuarlı
+    print("\U0001F914")  # 🤔 Düşünmek
+    print("\U0001F92B")  # 🤫 Sus işareti
+    print("\U0001F92D")  # 🤭 El ağızda
+    print("\U0001F921")  # 🤡 Palyaço
+    print("\U0001F920")  # 🤠 Kovboy
+
+
+
+if __name__ == "__main__":
+    print("Unicode Karakterleri ile Emoji Yazdırma:")
+    print_unicode_emojis()
+
